722268c0335aaf525ba5fde08afa24727b80eacd
Ranjithkandhan <ranjithcse14501505@gmail.com>
2018-09-28 19:25:24 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'userNotFound' : 'User not found',
+  'passwordNotMatch' : 'Password not match',
+  'missingCredential' : 'Missing credentials',
+  'accountExist' : "Account already exist"
+};
