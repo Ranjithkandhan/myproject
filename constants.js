bf7ef5522eb0bce318b834f19cfe2addbf422b90
Ranjithkandhan <ranjithcse14501505@gmail.com>
2018-09-28 19:29:55 +0530

    Delete constants.js

--- constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'userNotFound' : 'User not found',
-  'passwordNotMatch' : 'Password not match',
-  'missingCredential' : 'Missing credentials',
-  'accountExist' : "Account already exist"
-};
